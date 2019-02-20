0614caac495aae36171a8163c0a28f52b3c3dced
thaprau <paulsson.95@live.se>
2019-02-20 10:33:09 +0100

    late

--- Assignment4/galsim.c
+++ Assignment4/galsim.c
@@ -16,6 +16,7 @@ typedef struct QuadTree {
         double posx;
         double posy;
         double side;
+        int nop;
         struct particle ** particles;
 } QT;
 
@@ -35,25 +36,6 @@ typedef struct particle {
     double brightness;
 } part;
 
-void force_x(part *p1, part *p2, double r, double G) {
- 
-
-    double force = - G * (p1->mass) * (p2->mass) * ((p1->x) - (p2->x)) /(
-    (r + epsilon)*(r + epsilon)*(r + epsilon));
-
-    p1->x_force = p1->x_force + force;
-    p2->x_force = p2->x_force - force;
-
-}
-
-void force_y(part *p1, part *p2, double r, double G) {
-
-    double force = - G * (p1->mass) * (p2->mass) * ((p1->y) - (p2->y)) / (
-    (r + epsilon)*(r + epsilon)*(r + epsilon));
-
-    p1->y_force = p1->y_force + force;
-    p2->y_force = p2->y_force - force;
-}
 
 void vel_update(part** particles, int N, double t) {
 
@@ -74,13 +56,40 @@ void pos_update(part** particles, int N, double t) {
     }
 
 }
+void centerOfMass(QT * node, int N){
+    double mass = 0;
+    double x = 0;
+    double y = 0;
+    if(N == 0) {
+        node->C_O_x = 0;
+        node->C_O_x = 0;
+
+    }else {
+
+        for(int i=0; i<N; i++) {
+            mass += node->particles[i]->mass;
+            x += node->particles[i]->mass*node->particles[i]->x;
+            y += node->particles[i]->mass*node->particles[i]->y;
+        }
+        node->C_O_x = x/(mass);
+        node->C_O_y = y/(mass);
+        node->mass = mass;
+    } 
+}
 
 void create_tree(QT *node, int N) {
-        QT * child = (QT *)malloc(4*sizeof(QT));
-        node->child1 = &child[0];
-        node->child2 = &child[1];
-        node->child3 = &child[2];
-        node->child4 = &child[3];
+        QT * child1 = (QT *)malloc(sizeof(QT));
+        QT * child2 = (QT *)malloc(sizeof(QT));
+        QT * child3 = (QT *)malloc(sizeof(QT));
+        QT * child4 = (QT *)malloc(sizeof(QT));
+        node->child1 = child1;
+        node->child2 = child2;
+        node->child3 = child3;
+        node->child4 = child4;
+        child1->mass = 0;
+        child2->mass = 0;
+        child3->mass = 0;
+        child4->mass = 0;
         int count1 = 0;
         int count2 = 0;
         int count3 = 0;
@@ -89,17 +98,17 @@ void create_tree(QT *node, int N) {
         double posx = node->posx;
         double posy = node->posy;
         node->child1->side = new_side;
-        node->child1->posx = posx;
-        node->child1->posy = posy;
+        node->child1->posx = posx-new_side/2;
+        node->child1->posy = posy-new_side/2;
         node->child2->side = new_side;
-        node->child2->posx = posx + new_side;
-        node->child2->posy = posy;
+        node->child2->posx = posx + new_side/2;
+        node->child2->posy = posy - new_side/2;
         node->child3->side = new_side;
-        node->child3->posx = posx;
-        node->child3->posy = posy + new_side;
+        node->child3->posx = posx - new_side/2;
+        node->child3->posy = posy + new_side/2;
         node->child4->side = new_side;
-        node->child4->posx = posx + new_side;
-        node->child4->posy = posy + new_side;
+        node->child4->posx = posx + new_side/2;
+        node->child4->posy = posy + new_side/2;
         
         part **part1 = (part**)malloc(N*sizeof(part*));
         part **part2 = (part**)malloc(N*sizeof(part*));
@@ -109,8 +118,8 @@ void create_tree(QT *node, int N) {
 
 
         for(int i=0; i<N; i++) {
-            if(node->particles[i]->y <= posy + new_side) {
-                if(node->particles[i]->x <= posx + new_side) {
+            if(node->particles[i]->y <= posy) {
+                if(node->particles[i]->x <= posx) {
                     part1[count1] = node->particles[i];
                     count1++;
                 } else {
@@ -118,7 +127,7 @@ void create_tree(QT *node, int N) {
                     count2++;
                 }
             } else {
-                if(node->particles[i]->x <= posx + new_side) {
+                if(node->particles[i]->x <= posx) {
                     part3[count3] = node->particles[i];
                     count3++;    
                 } else {
@@ -131,7 +140,15 @@ void create_tree(QT *node, int N) {
         node->child2->particles = part2;
         node->child3->particles = part3;
         node->child4->particles = part4;
-
+        node->child1->nop = count1;
+        node->child2->nop = count2;
+        node->child3->nop = count3;
+        node->child4->nop = count4;
+        
+        centerOfMass(node->child1, count1);
+        centerOfMass(node->child2, count2);
+        centerOfMass(node->child3, count3);
+        centerOfMass(node->child4, count4);
         
         if(count1 > 1) {
             create_tree(node->child1, count1);
@@ -145,21 +162,79 @@ void create_tree(QT *node, int N) {
         if(count4 > 1) {
             create_tree(node->child4, count4);
         }
+    
 }
 
-void centerOFMass(QT * node, int N){
-    double mass = 0;
-    double x = 0;
-    double y = 0;
-    for(int i=0; i<N; i++) {
-        mass += node->particles[i]->mass;
-        x += node->particles[i]->mass*node->particles[i]->x;
-        y += node->particles[i]->mass*node->particles[i]->y;
+void free_tree(QT * node){
+    if(node->nop <=1) {
+        //free(node->particles);
+        free(node);
+        return;
     }
-    node->C_O_x = x/(mass);
-    node->C_O_y = y/(mass);
-    node->mass = mass;
+    else {
+        free_tree(node->child1);
+        free_tree(node->child2);
+        free_tree(node->child3);
+        free_tree(node->child4);
+        //free(node->particles);
+        free(node);
+    }
+}
+
+void force(part * particle, QT * node, double theta, double G){
+    if(particle->x == node->C_O_x && particle->y == node->C_O_y) {
+        //printf("Jämför med sig själv tredje \n");
+        return;
+    }
     
+    else if(node->nop == 0) {
+        
+        //printf("Should be first print and fouth\n");
+        return;
+    }
+    else if(node->nop ==1) {
+        //printf("Should be second print\n");
+        double r  = sqrt((particle->x-node->C_O_x)*(particle->x-node->C_O_x) + (particle->y-node->C_O_y)*(particle->y-node->C_O_y));
+
+        //double forcex = - G * (particle->mass) * (node->mass) * ((particle->x) - (node->C_O_x)) /(
+        //(r + epsilon)*(r + epsilon)*(r + epsilon));
+        double constant = - G * (particle->mass) * (node->mass)  /(
+        (r + epsilon)*(r + epsilon)*(r + epsilon)); 
+        double test = (particle->x - node->C_O_x);
+        double forcex = (particle->x - node->C_O_x) * constant;
+        //double forcey = - G * (particle->mass) * (node->mass) * ((particle->y) - (node->C_O_y)) /(
+        //(r + epsilon)*(r + epsilon)*(r + epsilon));
+        double forcey = (particle->y - node->C_O_y) * constant;
+
+        particle->x_force += forcex;
+        particle->y_force += forcey;
+        return;
+    }
+    else if((node->side)/sqrt((node->posx -particle->x)*(node->posx -particle->x) + (node->posy -particle->y)*(node->posy -particle->y)) <= theta) {
+        
+        //double r  = sqrt((particle->x-node->C_O_x)*(particle->x-node->C_O_x) + (particle->y-node->C_O_y)*(particle->y-node->C_O_y));
+        
+        double forcex = - G * (particle->mass) * (node->mass) * ((particle->x) - (node->posx)) /(
+        (sqrt((particle->x-node->C_O_x)*(particle->x-node->C_O_x) + (particle->y-node->C_O_y)*(particle->y-node->C_O_y)) + epsilon)*
+        (sqrt((particle->x-node->C_O_x)*(particle->x-node->C_O_x) + (particle->y-node->C_O_y)*(particle->y-node->C_O_y)) + epsilon)*
+        (sqrt((particle->x-node->C_O_x)*(particle->x-node->C_O_x) + (particle->y-node->C_O_y)*(particle->y-node->C_O_y)) + epsilon));
+        double forcey = - G * (particle->mass) * (node->mass) * ((particle->y) - (node->posy)) /(
+        (sqrt((particle->x-node->C_O_x)*(particle->x-node->C_O_x) + (particle->y-node->C_O_y)*(particle->y-node->C_O_y)) + epsilon)*
+        (sqrt((particle->x-node->C_O_x)*(particle->x-node->C_O_x) + (particle->y-node->C_O_y)*(particle->y-node->C_O_y)) + epsilon)*
+        (sqrt((particle->x-node->C_O_x)*(particle->x-node->C_O_x) + (particle->y-node->C_O_y)*(particle->y-node->C_O_y)) + epsilon));
+
+        particle->x_force += forcex;
+        particle->y_force += forcey;
+        
+        return;
+    }
+    else {
+        force(particle, node->child1, theta, G);
+        force(particle, node->child2, theta, G);
+        force(particle, node->child3, theta, G);
+        force(particle, node->child4, theta, G);
+        return;
+    }
 }
 
 
@@ -168,7 +243,7 @@ void centerOFMass(QT * node, int N){
 
 int main(int argc, char* argv[]) {
 
-    if (argc != 6) {
+    if (argc != 7) {
         printf("Wrong number of inputs");
         return -1;
     }
@@ -224,31 +299,24 @@ int main(int argc, char* argv[]) {
 
         while(t <= nsteps){
             ClearScreen();
-            printf("Loop nr %d \n", t);
 
-            for(int i = 0; i < N; i++) 
-            {
-                array[i]->y_force = 0;
-                array[i]->x_force = 0;
-            }
-
+            QT * root = (QT*)malloc(sizeof(QT));
+            root->posx = 0.5;
+            root->posy = 0.5;
+            root->side = 1;
+            root->particles = array;
+    
+            create_tree(root, N);
+            //printf("Loop nr --- %d\n", t);
             for(int i = 0; i < N; i++)  
             {
-                
                 DrawCircle(array[i]->x, array[i]->y, W, L, 0.01, 0);
-
-                for(int j = i+1; j < N; j++)
-                {
-                    double r = sqrt(pow((array[i]->x)-(array[j]->x), 2) + pow((array[i]->y)-(array[j]->y), 2));
-                    force_x(array[i], array[j], r, G );
-                    force_y(array[i], array[j], r, G);
-
-                }
+                force(array[i], root, theta_max, G);
+            //printf("particle nr %d force y : %lf\n", i, array[i]->y_force);
             }
-
-
             vel_update(array, N, delta_t);
             pos_update(array, N, delta_t);
+            t +=1;  
             
             Refresh();
             usleep(3000);
@@ -262,26 +330,32 @@ int main(int argc, char* argv[]) {
     // Loop without graphics
     else {
 
-
-        while(t < nsteps){}
+            
+        while(t < nsteps){
+            QT * root = (QT*)malloc(sizeof(QT));
+            root->posx = 0.5;
+            root->posy = 0.5;
+            root->side = 1;
+            root->particles = array;
+            root->nop = N;
+            //printf("Loop nr %d \n", t);
+            create_tree(root, N);
             //printf("Loop nr --- %d\n", t);
             for(int i = 0; i < N; i++)  
-            {
-                for(int j = i+1; j < N; j++)
-                {
-                    //printf("particle nr %d x : %lf\n", i, (array[i]->x-array[j]->x)*(array[i]->x-array[j]->x));
-                    double r = sqrt(
-                        (array[i]->x-array[j]->x)*(array[i]->x-array[j]->x) + (array[i]->y-array[j]->y)*(array[i]->y-array[j]->y)
-                        );
-                    //printf("r : %lf \n", r);
-                    force_x(array[i], array[j], r, G);
-                    force_y(array[i], array[j], r, G);
-                }
+            {   
+                //printf("PARICLE NUMER ----------------- %d\n", i);
+                //printf("x pos -- %lf \n", array[i]->x);
+                force(array[i], root, theta_max, G);
+                //printf("force %lf \n", array[i]->x_force);
             //printf("particle nr %d force y : %lf\n", i, array[i]->y_force);
             }
             vel_update(array, N, delta_t);
             pos_update(array, N, delta_t);
+            //printf("x pos after pos update -- %lf \n", array[0]->x);
             t +=1;
+            free_tree(root);
+            //printf("x pos after pos update free -- %lf \n", array[0]->x);
+            
         }                                
     }
 
